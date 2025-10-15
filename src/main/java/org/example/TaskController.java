438ee85a3fcfe18ef69a201976e53754e5de1e4b
Guilherme Arenas <guilhermearenas@hotmail.com>
2025-10-15 15:45:47 -0300

    Initial commit

---
+++ src/main/java/org/example/TaskController.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskService.getAllTasks();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Task> getTaskById(@PathVariable Long id) {
+        return taskService.getTaskById(id);
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskService.createTask(task);
+    }
+
+    @PutMapping("/{id}")
+    public Task updateTask(@PathVariable Long id, @RequestBody Task task) {
+        return taskService.updateTask(id, task);
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean deleteTask(@PathVariable Long id) {
+        return taskService.deleteTask(id);
+    }
+}
